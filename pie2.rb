7869eaa87d2f3091246fa1161bf3e7a3991366ec
Sarah Allen <sarah@ultrasaurus.com>
2010-08-29 16:11:06 +0900

    pie2 getting closer

--- pie2.rb
+++ pie2.rb
@@ -1,22 +1,30 @@
 
 class Pie
-  class PlaceFactory
+  attr_accessor :places
+  class Places < Array
     def method_missing name, *args
       puts "making a #{name} with #{args[0].inspect}"
-
+      self << {name.to_sym => args[0]}
     end
   end
 
   def create_places(&block)
-    @place_factory ||= PlaceFactory.new
-    @place_factory.instance_eval(&block)
+    @places ||= Places.new
+    @places.instance_eval(&block)
+  end
+
+  def image(image_hash)
+    @images ||= {}
+    puts "--- set image"
+    puts image_hash.inspect
+    @images.merge!(image_hash)
   end
 
 end
 
 def make_pie(&block)
-  my_pie = Pie.new
-  my_pie.instance_eval(&block)
+  $pie = Pie.new
+  $pie.instance_eval(&block)
 end
 
 make_pie do
@@ -25,4 +33,8 @@ make_pie do
     building description:"ookina biru"
     tower description:"ookina towa"
   end
+
+  image :ship => "images/big_ship.jpg", 
+        :building => "images/building.jpg", 
+        :tower => "images/tokyo_tower.jpg" 
 end
