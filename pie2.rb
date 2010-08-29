7fb9aac56b26afff9f6b51ede128ae8f1ec54453
Sarah Allen <sarah@ultrasaurus.com>
2010-08-29 16:45:06 +0900

    OMG!  It kinda works

--- pie2.rb
+++ pie2.rb
@@ -2,6 +2,17 @@ require 'sinatra/base'
 
 class Pie 
   class WebApp < Sinatra::Base
+    set :root, File.dirname(__FILE__)
+
+    get '/' do
+      "hello"
+    end
+
+    get '/:place_name' do
+      name = params[:place_name]
+      $current = name unless name.nil?
+      erb :image_page
+    end
   end
 
   at_exit { puts "exit"; WebApp.run! }
@@ -26,6 +37,20 @@ class Pie
     @images.merge!(image_hash)
   end
 
+ def current_image
+    puts "------- current_image"
+    puts $current.inspect
+    puts @images.inspect
+    @images[$current.to_sym]
+  end
+
+  def current_description
+    puts "------- current_description"
+    puts $current
+    puts @places
+    place = @places[$current.to_sym]
+    place[:description] unless place.nil?
+  end
 end
 
 def make_pie(&block)
