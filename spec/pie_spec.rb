e0c5136064a74512cfd101b5ee408e557891f821
Sarah Allen <sarah@ultrasaurus.com>
2010-08-30 19:14:59 +0900

    better path handling for require

--- spec/pie_spec.rb
+++ spec/pie_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/pie')
+require File.join(File.expand_path(File.dirname(__FILE__)), "pie")
 
 describe "making pie" do
   describe "image declaration" do
