d52b789219fd49450d3667be03dc2b2ac7d36e95
Sarah Allen <sarah@ultrasaurus.com>
2010-08-31 14:31:05 +0900

    some Japanese text:

--- book.rb
+++ book.rb
@@ -1,3 +1,4 @@
+#encoding:UTF-8
 $LOAD_PATH << File.dirname(__FILE__)
 require 'lib/pie'
 
@@ -8,7 +9,7 @@ end
 
 make_pie do
   create_places do
-    ship description:"ookina fune"
+    ship description:"大きな船"
     building description:"ookina biru"
     tower description:"ookina towa"
   end
