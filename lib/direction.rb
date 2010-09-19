3295dc610d4f16aa4452637b4f917750db1c6f6b
Rich Kilmer <rich@infoether.com>
2010-09-19 16:11:45 -0400

    add dead_end back in but make it just a direction with no opposite, remove DeadEnd class

--- lib/direction.rb
+++ lib/direction.rb
@@ -20,28 +20,39 @@ module Pie
     dir
   end
   
+  def dead_end(name)
+    direction(name)
+  end
+  
   def north
     @north ||= direction(Pie.localized(:go_north), Pie.localized(:go_south))
   end
+  
   def south
     @south ||= direction(Pie.localized(:go_south), Pie.localized(:go_north))
   end
+  
   def east
     @east ||= direction(Pie.localized(:go_east), Pie.localized(:go_west))
   end
+  
   def west
     @west ||= direction(Pie.localized(:go_west), Pie.localized(:go_east))
   end
+  
   def north!
-    direction(north)
+    dead_end(north)
   end
+  
   def south!
-    direction(south)
+    dead_end(south)
   end
+  
   def east!
-    direction(east)
+    dead_end(east)
   end
+  
   def west!
-    direction(west)
+    dead_end(west)
   end
 end
