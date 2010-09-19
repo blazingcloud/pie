4a2a71fb23357a0b06d84b44f2cbfa9e7706ba25
Rich Kilmer <rich@infoether.com>
2010-09-19 11:45:39 -0400

    refactor for new api

--- book.rb
+++ book.rb
@@ -1,17 +1,13 @@
 #encoding:UTF-8
-$LOAD_PATH << File.dirname(__FILE__)
-require 'lib/pie'
 
-make_pie do
-  create_places do
-    ship description:"大きな船"
-    building description:"ookina biru"
-    tower description:"ookina tawaa"
-  end
+template :game_screen
 
-  image ship:"images/big_ship.jpg", 
-        building:"images/building.jpg", 
-        tower:"images/tokyo_tower.jpg" 
-end
+place ship:"大きな船"
+place building:"ookina biru"
+place tower:"ookina tawaa"
+
+image ship:"images/big_ship.jpg", 
+      building:"images/building.jpg", 
+      tower:"images/tokyo_tower.jpg" 
 
 
