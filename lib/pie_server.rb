eae43036b56d952ce9c43db64d3d9265f81460de
Sarah Allen <sarah@ultrasaurus.com>
2010-10-08 08:51:15 -0700

    fixup pie_server for setting data

--- lib/pie_server.rb
+++ lib/pie_server.rb
@@ -8,11 +8,12 @@ class Pie::WebApp < Sinatra::Base
   end
 
   get '/:place_name' do
-    puts "settings.pie_data = #{settings.pie_data.inspect}"
-    
-    pie ||= settings.pie_data
+    puts "env= #{request.env.inspect}"
+    puts "request.env[PIE_DATA]= #{request.env["PIE_DATA"].inspect}"
     pie ||= request.env["PIE_DATA"]
+    pie ||= settings.pie_data if settings.respond_to? :pie_data
     puts "pie = #{pie.inspect}"
+    if pie
       name = params[:place_name].to_sym
       pie.current_place(name) unless name.nil?
       puts "current place name is #{pie.current_place.name}"
@@ -21,6 +22,9 @@ class Pie::WebApp < Sinatra::Base
       puts "current links are #{pie.current_place.paths.inspect}"
       puts "displaying template: #{pie.template.inspect}"
       erb pie.template, {}, {:pie => pie}
+    else 
+      puts "NO PIE! AAAAAH!"
+    end
   end
 
 end
