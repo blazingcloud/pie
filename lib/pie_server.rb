52264d418abd64f158187e3e9aa38e6aa200e879
Matthew <pelargir@gmail.com>
2010-11-12 17:04:03 -0600

    visiting root url uses first place in list

--- lib/pie_server.rb
+++ lib/pie_server.rb
@@ -3,18 +3,14 @@ require 'sinatra/base'
 class Pie::WebApp < Sinatra::Base
   set :root, File.join(File.expand_path(File.dirname(__FILE__)), "..")
 
-  get '/' do
-    "hello"
-  end
-
-  get '/:place_name' do
+  get '/*' do
     puts "env= #{request.env.inspect}"
     puts "request.env[PIE_DATA]= #{request.env["PIE_DATA"].inspect}"
-    pie ||= request.env["PIE_DATA"]
-    pie ||= settings.pie_data if settings.respond_to? :pie_data
+    pie ||= request.env["PIE_DATA"] || settings.pie_data
     puts "pie = #{pie.inspect}"
     if pie
-      name = params[:place_name].to_sym
+      splat = params[:splat].find { |e| e.length > 0 }
+      name = (splat || settings.pie_data.places.first.first).to_sym
       pie.current_place(name) unless name.nil?
       puts "current place name is #{pie.current_place.name}"
       puts "current place is #{pie.current_place.description}"
